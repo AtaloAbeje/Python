2ef8c4f43f0893aa526efed595e9606eb6f5aeaf
Atalo Abeje <44359191+AtaloAbeje@users.noreply.github.com>
2018-12-14 12:57:36 +0200

    Python

--- Home_work/Object Oriented/TaskStudentClass.01.py
+++ Home_work/Object Oriented/TaskStudentClass.01.py
@@ -6,7 +6,7 @@ class Student:
     def __init__(self, mark, age):
         self.mark = mark
         self.age = age
-        Student.sum_students+=1            # summarize of students
+        Student.sum_students += 1            # summarize of students
 
         Student.sum_age += self.age        # summarize of age students
         Student.sum_marks += self.mark     # summarize of marks students
@@ -20,14 +20,14 @@ class Student:
         print("avg age students: {}".format(class_level.sum_age / class_level.sum_students))
 
 # main()
-s1 = Student(80 ,22) # list of students
+s1 = Student(80, 22) # list of students
 s2 = Student(85, 23)
 s3 = Student(90, 24)
 s4 = Student(95, 25)
 
-print("sum students:",Student.sum_students)
-print("sum age students:",Student.sum_age)
-print("sum marks students:",Student.sum_marks)
+print("sum students:", Student.sum_students)
+print("sum age students:", Student.sum_age)
+print("sum marks students:", Student.sum_marks)
 print("------------------------")
 Student.avg_age()
 Student.avg_marks()
